1930c07a72b2a639166d19309f468c410188e1e5
BriefNCounter <marcusdurston@yandex.com>
2023-08-27 21:51:46 +0200

    aliases.01

--- home.nix
+++ home.nix
@@ -27,31 +27,31 @@ programs.bash = {
 
 
 shellAliases = {
-              #post= "cd ~/Websites/Sites/nixs.info/content/english/posts";                
-    ▍      #webs = "cd ~/Websites/Sites/nixs.info && npm run dev";                      
-    ▍      #n = "sudo nvim";                                                            
-    ▍      #nv = "nvim";                                                                
-    ▍      #webd = "cd ~/Websites/Sites/nixs.info";                                     
-            cf = "cd /etc/nixos && sudo hx configuration.nix";                           
-            fl = "cd /etc/nixos && sudo hx flake.nix";                                   
+          #post= "cd ~/Websites/Sites/nixs.info/content/english/posts";                
+    ▍     #webs = "cd ~/Websites/Sites/nixs.info && npm run dev";                      
+    ▍     #n = "sudo nvim";                                                            
+    ▍     #nv = "nvim";                                                                
+    ▍     #webd = "cd ~/Websites/Sites/nixs.info";                                     
+          cf = "cd /etc/nixos && sudo hx configuration.nix";                           
+          fl = "cd /etc/nixos && sudo hx flake.nix";                                   
           hm = "cd /etc/nixos && sudo hx home.nix";                                    
-     ▍      rb = "sudo nixos-rebuild --flake .#My_Nix switch";                           
-            c = "clear";                                                                 
-            g0 = "sudo git init";                                                        
-            g1 = "sudo git add .";                                                       
-            g3 = "sudo git push -u origin main";                                         
-            xx = "reboot";                                                               
-           s = "sudo -s";                                                               
-            pd = "cd ~/Programming/Python";                                              
-            lq = "ls -ha";                                                               
-     ▍      test = "sudo nixos-rebuild test --flake .#My_Nix";                           
-     ▍      check = "sudo nix flake check";                                              
-     ▍      nx = "cd /etc/nixos"; 
-                 man = "man home-configuration.nix";
-      maj = "sudo nix flake update && rb";      
+     ▍    rb = "sudo nixos-rebuild --flake .#My_Nix switch";                           
+          c = "clear";                                                                 
+          g0 = "sudo git init";                                                        
+          g1 = "sudo git add .";                                                       
+          g3 = "sudo git push -u origin main";                                         
+          xx = "reboot";                                                               
+          s = "sudo -s";                                                               
+          pd = "cd ~/Programming/Python";                                              
+          lq = "ls -ha";                                                               
+     ▍    test = "sudo nixos-rebuild test --flake .#My_Nix";                           
+     ▍    check = "sudo nix flake check";                                              
+     ▍    nx = "cd /etc/nixos"; 
+          man = "man home-configuration.nix";
+          maj = "sudo nix flake update && rb";      
 
 };
-
+};
   home.stateVersion = "23.05";
 
   # Let home Manager install and manage itself.
