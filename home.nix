01b6eb910afb2acf734535d21748c7b531b10191
BriefNCounter <marcusdurston@yandex.com>
2023-08-27 22:31:13 +0200

    nixvim.02

--- home.nix
+++ home.nix
@@ -1,12 +1,48 @@
-{ config, pkgs, ... }:
+{config, pkgs, ... }:
 
 {
   home.username = "marcus";
   home.homeDirectory = "/home/marcus";
 
+programs.nixvim = {
+    enable = true;
+colorschemes.gruvbox.enable = true;
+    plugins.lightline.enable = true;
+    # Configure neovim options...
+    options = {
+      relativenumber = true;
+      incsearch = true;
+    };
+
+    # ...mappings...
+    maps.normal = {
+      "<C-s>" = ":w<CR>";
+      "<esc>" = { action = ":noh<CR>"; silent = true; };
+    };
+
+    # ... and plugins
+    plugins = {
+      telescope.enable = true;
+      harpoon = {  # Hi Prime :)
+        enable = true;
+        keymaps.addFile = "<leader>a";
+      };
+    };
+  };
+
 
   home.packages = with pkgs; [
   tree
+  nnn
+  exa
+  ripgrep
+  neofetch
+  fzf
+  dnsutils
+  gawk
+  nix-output-monitor
+  glow
+  lsof
   mailspring
   nodejs_20
   go
@@ -25,13 +61,28 @@ programs.bash = {
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
 
+# Git config using Home Manager modules
+#  programs.git = {
+#    enable = true;
+#    userName = "BriefNCounter";
+#    userEmail = "marcusdurston@yandex.com";
+#    aliases = {
+#      st = "status";
+#    };
+#  };
+
+
+
 shellAliases = {
-      nixs = "cd ~/Websites/Sites/nixs.info && npm run dev";
-      nix = "cd ~/Websites/Sites/nixs.info";
+      #post= "cd ~/Websites/Sites/nixs.info/content/english/posts";
+      #webs = "cd ~/Websites/Sites/nixs.info && npm run dev";
+      #n = "sudo nvim";
+      #nv = "nvim";
+      #webd = "cd ~/Websites/Sites/nixs.info";
       cf = "cd /etc/nixos && sudo hx configuration.nix";
       fl = "cd /etc/nixos && sudo hx flake.nix";
       hm = "cd /etc/nixos && sudo hx home.nix";
-      rb = "cd /etc/nixos && sudo sudo nixos-rebuild --flake .#My_Nix switch";
+      rb = "sudo nixos-rebuild --flake .#My_Nix switch";
       c = "clear";
       g0 = "sudo git init";
       g1 = "sudo git add .";
@@ -40,13 +91,18 @@ shellAliases = {
       s = "sudo -s";
       pd = "cd ~/Programming/Python";      
       lq = "ls -ha";
+      test = "sudo nixos-rebuild test --flake .#My_Nix";      
+      check = "sudo nix flake check";
+      nx = "cd /etc/nixos";
+      man = "man home-configuration.nix";
+      maj = "sudo nix flake update && rb";
 
-
-
+            
       };
+
 };
 
-    home.stateVersion = "23.05";
+  home.stateVersion = "23.05";
 
   # Let home Manager install and manage itself.
   programs.home-manager.enable = true;
