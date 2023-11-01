23d6907a7c62214cc684cdd0e56db1edb1f8b211
BriefNCounter <marcusdurston@yandex.com>
2023-11-01 11:07:49 +0100

    aha

--- configuration.nix
+++ configuration.nix
@@ -146,6 +146,7 @@ nix.settings.trusted-users = [ "marcus" ];
   nushellFull
   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   wget
+  aha
   htop
 	git  
   brave
